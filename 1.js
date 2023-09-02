11a5da124203b41f376a14cdff7a029a099ec24f
Softgod4 <softgod2005@gmail.com>
2023-09-02 15:53:30 +0300

    ну сделал задания там

--- 1.js
+++ 1.js
@@ -1,7 +1,10 @@
-console.log(evenOrOdd(5))
-
-function evenOrOdd(number) {
-  if (number % 2 <= 0) {return "Even"} else {return "Odd"}
+function moveZeros(arr) {
+  const curret = arr.reduce((acc, curr) => {
+    if (curr === 0) {
+      arr.unshift(curr)
+    }
+  }, 0)
+  return curret
 }
 
-console.log(-7 % 2)
+console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
---
+++ 1.py
@@ -0,0 +1,44 @@
+# def move_zeros(lst):
+#     arraynum = 0
+#     for nul in lst:
+#         if nul == 0:
+#             lst.remove(nul)
+#             arraynum += 1
+
+#     for i in range(arraynum):
+#         lst.append(0)
+#     return lst          
+
+# print(move_zeros([1, 0, 1, 0]))
+
+# def move_zeros(lst):
+#     arraynum = 0
+#     for i in range(len(lst)):
+#         if i < len(lst) and lst[i] == 0:
+#             lst.pop(i)
+#             arraynum += 1
+
+#     for i in range(arraynum):
+#         lst.append(0)
+#     return lst
+
+# print(move_zeros([9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0] ))
+
+def move_zeros(lst):
+    arrraynum = []
+    nulcout = 0
+    for num in lst:
+        if num != 0:
+            arrraynum.append(num)
+        else:
+            nulcout += 1
+
+    for i in range(nulcout):
+        arrraynum.append(0)
+        
+    return arrraynum
+
+print(move_zeros([9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0] ))
+
+
+
