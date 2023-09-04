f57c59836765f2784ab1cdfc263830a8bf7bacf2
Softgod4 <softgod2005@gmail.com>
2023-09-04 19:24:58 +0300

    изменена 1 задача

--- 1.py
+++ 1.py
@@ -1,29 +1,3 @@
-# def move_zeros(lst):
-#     arraynum = 0
-#     for nul in lst:
-#         if nul == 0:
-#             lst.remove(nul)
-#             arraynum += 1
-
-#     for i in range(arraynum):
-#         lst.append(0)
-#     return lst          
-
-# print(move_zeros([1, 0, 1, 0]))
-
-# def move_zeros(lst):
-#     arraynum = 0
-#     for i in range(len(lst)):
-#         if i < len(lst) and lst[i] == 0:
-#             lst.pop(i)
-#             arraynum += 1
-
-#     for i in range(arraynum):
-#         lst.append(0)
-#     return lst
-
-# print(move_zeros([9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0] ))
-
 def move_zeros(lst) -> int:
     arrraynum = []
     nulcout = 0
