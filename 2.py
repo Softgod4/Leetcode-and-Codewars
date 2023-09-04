0e64468e09d41eafae14bbe233c5d38835621419
Softgod <55946703+Softgod4@users.noreply.github.com>
2023-09-04 17:54:35 +0300

    что-то делается

--- 2.py
+++ 2.py
@@ -1,6 +1,12 @@
-def scramble(s1, s2):
-    sortarray = []
+def scramble(s1, s2) -> bool:
+    lists2 = list(s2)
     for i in range(len(s1)):
-        ...
+        for a in range(len(lists2)):
+            if lists2[a] == s1[i]:
+                lists2.pop(a)
+                print(lists2)
+            else:
+                return True
 
-print(scramble('cedewaraaossoqqyt', 'codewars'))
+print(scramble('katas', 'steak')) #False
+print(scramble('scriptingjava', 'javascript')) #True
