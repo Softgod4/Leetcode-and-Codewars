127228621e5748378e7f63e12b3ecb89cd4630df
Softgod4 <softgod2005@gmail.com>
2023-09-04 16:14:57 +0300

    сделана еще одна задача

--- 1.py
+++ 1.py
@@ -24,7 +24,7 @@
 
 # print(move_zeros([9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0] ))
 
-def move_zeros(lst):
+def move_zeros(lst) -> int:
     arrraynum = []
     nulcout = 0
     for num in lst:
