684209f894aa78ac8a304e04938d90810b29b2fe
Softgod <55946703+Softgod4@users.noreply.github.com>
2023-09-04 17:21:56 +0300

    делаю задачу

--- 2.py
+++ 2.py
@@ -1,10 +1,6 @@
 def scramble(s1, s2):
     sortarray = []
     for i in range(len(s1)):
-        for a in range(len(s2)):
-            if s2[a] == s1[i]:
-                sortarray.append(s2[a])
-                break
-    return sortarray
+        ...
 
-print(scramble('katas', 'steak'))
+print(scramble('cedewaraaossoqqyt', 'codewars'))
